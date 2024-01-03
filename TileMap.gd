04783af128621c117a685be143ea0056a49b1b74
Goldenlion5648 <coboudinot@gmail.com>
2024-01-02 21:07:57 -0500

    add back code to fill area

--- TileMap.gd
+++ TileMap.gd
@@ -12,12 +12,11 @@ const main_source = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	#for y in range(3):
-		#for x in range(3):
-			#set_cell(layers.level0, Vector2i(2 + x, 2 + y), main_source, green_block_atlas_pos)
-	#
-	#set_cell(layers.level1, Vector2i(2, 2), main_source, blue_block_atlas_pos)
+	for y in range(3):
+		for x in range(3):
+			set_cell(layers.level0, Vector2i(2 + x, 2 + y), main_source, green_block_atlas_pos)
+	
+	set_cell(layers.level1, Vector2i(2, 2), main_source, blue_block_atlas_pos)
 		
 
 
